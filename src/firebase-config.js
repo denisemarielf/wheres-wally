519ee5425cbc97394b6c06eedd9c679523e165a2
Denise <denisemarielf@gmail.com>
2022-07-12 23:00:41 -0300

    src/index.js
    Format with Prettier

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAh7Un3RGhme85bROYmxYoETR75pydLLOI",
-    authDomain: "wheres-wally-game.firebaseapp.com",
-    projectId: "wheres-wally-game",
-    storageBucket: "wheres-wally-game.appspot.com",
-    messagingSenderId: "682674230381",
-    appId: "1:682674230381:web:01a0f82b0bbc03269e9b64"
-  };
+  apiKey: "AIzaSyAh7Un3RGhme85bROYmxYoETR75pydLLOI",
+  authDomain: "wheres-wally-game.firebaseapp.com",
+  projectId: "wheres-wally-game",
+  storageBucket: "wheres-wally-game.appspot.com",
+  messagingSenderId: "682674230381",
+  appId: "1:682674230381:web:01a0f82b0bbc03269e9b64",
+};
 
-  const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-  export const db = getFirestore()
+export const db = getFirestore();
